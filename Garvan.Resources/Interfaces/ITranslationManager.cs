0507fa21c15f485b176fdc87d56e7677dc125cf1
Konstantin Todorov <kztodorov@outlook.com>
2017-12-09 01:14:20 +0200

    Implemented the all partial views of the main page
     - There is still work to be done

---
+++ Garvan.Resources/Interfaces/ITranslationManager.cs
@@ -0,0 +1,12 @@
+﻿using Garvan.Resources.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Garvan.Resources.Interfaces
+{
+    public interface ITranslationManager
+    {
+        string GetResource(string resourceKey, TranslationLanguage language);
+    }
+}
