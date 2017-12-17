f66d091a50633b18c28e4e9a4fb413e0ee50a86b
Konstantin Todorov <kztodorov@outlook.com>
2017-12-17 05:28:26 +0200

    Fixes selected chapter marking

--- Garvan.Web/wwwroot/js/site.js
+++ Garvan.Web/wwwroot/js/site.js
@@ -9,9 +9,29 @@ $("a.chapter-link").on('click', function (event) {
         $('html, body').animate({
             scrollTop: $(hash).offset().top - 70
         }, 800);
-
-        debugger;
-        $("a.chapter-nav-link").removeClass("selected-chapter");
-        $("a.chapter-nav-link[href='" + hash + "']").addClass("selected-chapter");
     }
 });
+
+$(window).scroll(function () {
+    $("a.chapter-nav-link").removeClass("selected-chapter");
+
+    var chapterLinks = $("a.chapter-nav-link");
+    for (var i = 0; i < chapterLinks.length; i++) {
+        var linkHash = chapterLinks[i].hash;
+        var elementIsVisible = isScrolledIntoView(linkHash);
+        if (elementIsVisible) {
+            $("a.chapter-nav-link[href='" + linkHash + "']").addClass("selected-chapter");
+            return;
+        }
+    }
+});
+
+function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
