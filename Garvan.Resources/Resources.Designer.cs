2e0ff01718a9f90fadeac3c9669f7552c8915db3
Konstantin Todorov <kztodorov@outlook.com>
2018-08-12 17:09:08 +0300

    Implement Accessories page

--- Garvan.Resources/Resources.Designer.cs
+++ Garvan.Resources/Resources.Designer.cs
@@ -106,6 +106,15 @@ namespace Garvan.Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Accessories.
+        /// </summary>
+        public static string Accessories {
+            get {
+                return ResourceManager.GetString("Accessories", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to All shipments to Bulgaria are delivered by Econt with options to pay on delivery and check the product before payment.
         /// </summary>
         public static string AllShipmentsToBulgariaContactsInfo {
@@ -367,6 +376,15 @@ namespace Garvan.Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Garvan Pad XL.
+        /// </summary>
+        public static string GarvanPadXL {
+            get {
+                return ResourceManager.GetString("GarvanPadXL", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to +359 88 732 1987.
         /// </summary>
         public static string GarvanPhone {
@@ -489,6 +507,87 @@ namespace Garvan.Resources {
         public static string OurDistributors {
             get {
                 return ResourceManager.GetString("OurDistributors", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Color.
+        /// </summary>
+        public static string PadColorLabel {
+            get {
+                return ResourceManager.GetString("PadColorLabel", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Black.
+        /// </summary>
+        public static string PadColorValue {
+            get {
+                return ResourceManager.GetString("PadColorValue", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Dimensions.
+        /// </summary>
+        public static string PadDimensionsLabel {
+            get {
+                return ResourceManager.GetString("PadDimensionsLabel", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 900x300 mm.
+        /// </summary>
+        public static string PadDimensionsValue {
+            get {
+                return ResourceManager.GetString("PadDimensionsValue", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Material.
+        /// </summary>
+        public static string PadMaterialLabel {
+            get {
+                return ResourceManager.GetString("PadMaterialLabel", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Smooth fabric and rubber base.
+        /// </summary>
+        public static string PadMaterialValue {
+            get {
+                return ResourceManager.GetString("PadMaterialValue", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Price.
+        /// </summary>
+        public static string PadPriceLabel {
+            get {
+                return ResourceManager.GetString("PadPriceLabel", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 60 BGN.
+        /// </summary>
+        public static string PadPriceValueNew {
+            get {
+                return ResourceManager.GetString("PadPriceValueNew", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 85 BGN.
+        /// </summary>
+        public static string PadPriceValueOld {
+            get {
+                return ResourceManager.GetString("PadPriceValueOld", resourceCulture);
             }
         }
         
---
+++ Garvan.Web/wwwroot/js/accessories.js
@@ -0,0 +1,40 @@
+﻿$("#main-image").mouseout(zoomOut);
+$("#main-image").mousemove(function (event) {
+    zoomIn(event);
+});
+
+function zoomIn(event) {
+    var $element = $("#accessories-overlay-image");
+    $element.css('display', 'inline-block');
+    var $img = $("#main-image");
+    var posX = event.offsetX ? (event.offsetX) : event.pageX - $img.css('offsetLeft');
+    var posY = event.offsetY ? (event.offsetY) : event.pageY - $img.css('offsetTop');
+    var imageElement = document.getElementById("main-image");
+    var realImageWidth = imageElement.naturalWidth;
+    var realImageHeight = imageElement.naturalHeight;
+    var imageHeightRatio = (realImageHeight / $img.outerHeight());
+    var imageWidthRatio = (realImageWidth / $img.outerWidth());
+    var backgroundPosition = ((-posX * imageWidthRatio) + 150) + "px " + ((-posY * imageHeightRatio) + 150) + "px";
+    $element.css('backgroundPosition', backgroundPosition);
+}
+
+function zoomOut() {
+    var $element = $("#accessories-overlay-image");
+    $element.css('display', 'none');
+}
+
+$(".image-picker-wrapper").click(function () {
+    var $imageClicked = $(this).find("img").first();
+    var imageSource = $imageClicked.attr('src');
+    $("#accessories-overlay-image").css('background-image', 'url(' + imageSource + ')');
+    $("#main-image").attr('src', imageSource);
+});
+
+$("#zoom-image-button").click(() => {
+    $("#outer-zoomed-image-wrapper").show();
+    $("#outer-zoomed-image-wrapper img").attr('src', $("#main-image").attr('src'));
+});
+
+$("#outer-zoomed-image-wrapper .close-button").click(() => {
+    $("#outer-zoomed-image-wrapper").hide();
+});
